5889d88a9071eb57f2a3d4281e36f3b03032a636
Gustav Svendsen <gsvendsen99@gmail.com>
2018-11-12 11:44:12 +0100

    Added onclick scripts for navbar

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
+const hamButton = document.querySelector(".hamburger-menu");
+const hamBar = document.querySelector(".bar");
+const menu = document.querySelector('.menu');
+const menuItems = document.querySelectorAll('.menu-item')
 
+hamButton.addEventListener("click", ()=>{
+  console.log("bla");
+  hamBar.classList.toggle("clicked");
+  menu.classList.toggle("open");
+});
+
+menuItems.forEach((menuItem) => {
+  menuItem.addEventListener("click", ()=>{
+
+    menuItems.forEach((menuItem) => {
+      menuItem.classList.remove("selected");
+    });
+    menuItem.classList.add("selected");
+
+  })
+});
