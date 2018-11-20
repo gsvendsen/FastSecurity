6eb67fd44de5f0ebdfe47f9f8b6fce25bf7711c7
Neyrin <isa.areschoug@gmail.com>
2018-11-20 14:07:27 +0100

    Added history content text

--- lang/en.php
+++ lang/en.php
@@ -11,6 +11,6 @@ return [
   'hero-header' => 'Introducing the future.',
   'hero-desc' => 'Sign up to our newsletter for exclusive offers, deals and release dates on the motorcycles of the next century.',
   'register' => 'Register',
-  'history-header' => 'History',
-  'history-cont' => 'Detta är mycket historia!'
+  'history-header' => 'History of the FS1',
+  'history-cont' => 'Saab started life in 1937 in Trollhättan, Sweden, as an aeroplane manufacturer to provide aircraft for the Swedish Air Force, just as Europe was steeling itself for another war. The company\'s full name was Svenska Aeroplan Aktiebolaget, giving the world the acronym Saab. The head office soon moved to the Swedish university town of Linköping, where this massive company has its largest operation today. It was there in 1945, amid various dreams of diversification, that a project to develop motor cars got underway. Today we are starting a new chapter. Fast Security, FS. Is the future motorcycle.'
 ];
