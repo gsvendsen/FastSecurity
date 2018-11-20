04846f05acc4b0b5bb8158f033bfcfc5e744fa15
Neyrin <isa.areschoug@gmail.com>
2018-11-20 15:43:44 +0100

    Added texts for Autonomus section and CallToAction

--- lang/en.php
+++ lang/en.php
@@ -11,6 +11,13 @@ return [
   'hero-header' => 'Introducing the future.',
   'hero-desc' => 'Sign up to our newsletter for exclusive offers, deals and release dates on the motorcycles of the next century.',
   'register' => 'Register',
+  'CTA-header' => 'Want to know more?',
+  'CTA-desc' => 'Sign up to our newsletter for exclusive offers, deals and release dates on the motorcycles of the next century.',
   'history-header' => 'History of the FS1',
-  'history-cont' => 'Saab started life in 1937 in Trollhättan, Sweden, as an aeroplane manufacturer to provide aircraft for the Swedish Air Force, just as Europe was steeling itself for another war. The company\'s full name was Svenska Aeroplan Aktiebolaget, giving the world the acronym Saab. The head office soon moved to the Swedish university town of Linköping, where this massive company has its largest operation today. It was there in 1945, amid various dreams of diversification, that a project to develop motor cars got underway. Today we are starting a new chapter. Fast Security, FS. Is the future motorcycle.'
+  'history-cont' => 'Saab started life in 1937 in Trollhättan, Sweden, as an aeroplane manufacturer to provide aircraft for the Swedish Air Force, just as Europe was steeling itself for another war. The company\'s full name was Svenska Aeroplan Aktiebolaget, giving the world the acronym Saab. The head office soon moved to the Swedish university town of Linköping, where this massive company has its largest operation today. It was there in 1945, amid various dreams of diversification, that a project to develop motor cars got underway. Today we are starting a new chapter. Fast Security, FS. Is the future motorcycle.', '',
+  'auto-header' => 'Fully Autonomous',
+  'auto-cont' => 'Autonomous cars combine a variety of sensors to perceive their surroundings, such as radar, computer vision, Lidar, sonar, GPS, odometry and inertial measurement units. A self-driving car, also known as a robot car, autonomous car, or driverless car is a vehicle that is capable of sensing its environment and moving with little or no human input.',
+  'break-header' => 'Breaking sensors',
+  'break-cont' => 'Hitting top speeds or hugging tight corners, the Lucid Air’s perfectly calibrated air springs, advanced damper technology, and low center of gravity deliver precision handling and a smooth ride.',
+
 ];
