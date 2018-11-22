aac8402883dd8addd6756a26b80894b498c5f319
Gustav Svendsen <gsvendsen99@gmail.com>
2018-11-22 16:56:31 +0100

    Additional fixes on engineering section

--- index.php
+++ index.php
@@ -259,7 +259,7 @@ if(!isset($_SESSION['lang'])){
     <div class="engineering-article">
       <div class="text-section">
         <p class="article-medium">The FS1 is engineered to perfection with the finest materials in mind, creating a fantastic experience combined with the luxorious comfort of leather seats.</p>
-        <div class="read-more">
+        <div class="read-more desktop">
           <a href="#">
             Read more
             <svg width="23" height="13" viewBox="0 0 23 13" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -272,17 +272,30 @@ if(!isset($_SESSION['lang'])){
       </div>
       <div class="stat-section">
         <div class="stats">
-          <p class="stats-text">uo to</p>
+          <p class="stats-text">up to</p>
           <p class="stats-info">360 km / h</p>
           <p class="stats-text">with</p>
           <p class="stats-info">600hp</p>
           <p class="stats-text">with turbo charge</p>
         </div>
-        <div class="stat-image">
-
+        <div class="image-section">
+          <div class="stat-image">
+          </div>
+          <p class="stats-text">Graph on the acceleration – FS1</p>
+        </div>
+        <div class="read-more mobile">
+          <a href="#">
+            Read more
+            <svg width="23" height="13" viewBox="0 0 23 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M1 6.60791L21.5263 6.60791" stroke="#7E8EAB" stroke-width="1.5" stroke-linecap="round"/>
+              <path d="M16.8923 2L21.6662 6.77387" stroke="#7E8EAB" stroke-width="1.5" stroke-linecap="round"/>
+              <path d="M16.8923 11.5479L21.6662 6.77399" stroke="#7E8EAB" stroke-width="1.5" stroke-linecap="round"/>
+            </svg>
+          </a>
         </div>
       </div>
     </div>
+    <img class="engineering-image left" src="resources/interior.png">
   </div>
   <!-- /ENGINEERING -->
 
