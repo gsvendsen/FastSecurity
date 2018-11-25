a5ecc8e58bd5d15c2be5db865de715455ab7a800
Neyrin <isa.areschoug@gmail.com>
2018-11-25 21:13:10 +0100

    Corrected typo

--- lang/en.php
+++ lang/en.php
@@ -14,12 +14,11 @@ return [
   'CTA-header' => 'Want to know more?',
   'CTA-desc' => 'Sign up to our newsletter for exclusive offers, deals and release dates on the motorcycles of the next century.',
   'history-header' => 'History of the FS1',
-  'history-cont' => 'Saab started life in 1937 in Trollhättan, Sweden, as an aeroplane manufacturer to provide aircraft for the Swedish Air Force, just as Europe was steeling itself for another war. The company\'s full name was Svenska Aeroplan Aktiebolaget, giving the world the acronym Saab. The head office soon moved to the Swedish university town of Linköping, where this massive company has its largest operation today. It was there in 1945, amid various dreams of diversification, that a project to develop motor cars got underway. Today we are starting a new chapter. Fast Security, FS. Is the future motorcycle.', '',
+  'history-cont' => 'Saab started life in 1937 in Trollhättan, Sweden, as an aeroplane manufacturer to provide aircraft for the Swedish Air Force, just as Europe was steeling itself for another war. The company\'s full name was Svenska Aeroplan Aktiebolaget, giving the world the acronym Saab. The head office soon moved to the Swedish university town of Linköping, where this massive company has its largest operation today. It was there in 1945, amid various dreams of diversification, that a project to develop motor cars got underway. Today we are starting a new chapter. Fast Security, FS. Is the future motorcycle.',
   'auto-header' => 'Fully Autonomous',
   'auto-cont' => 'Autonomous cars combine a variety of sensors to perceive their surroundings, such as radar, computer vision, Lidar, sonar, GPS, odometry and inertial measurement units. A self-driving car, also known as a robot car, autonomous car, or driverless car is a vehicle that is capable of sensing its environment and moving with little or no human input.',
   'break-header' => 'Breaking sensors',
   'break-cont' => 'Hitting top speeds or hugging tight corners, the Lucid Air’s perfectly calibrated air springs, advanced damper technology, and low center of gravity deliver precision handling and a smooth ride.',
   'phone-header' => 'Smartphone App',
-  'phone-content' => 'Adjust your special bike to your liking using our smartphone app that you can connect to the FS1. Calibrate temperature, torque and other special features in order to make your FS1 suited best for you.',
-
+  'phone-content' => 'Adjust your special bike to your liking using our smartphone app that you can connect to the FS1. Calibrate temperature, torque and other special features in order to make your FS1 suited best for you.'
 ];
