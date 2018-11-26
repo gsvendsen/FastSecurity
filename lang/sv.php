8eacdb9538fbcb1a54e6d433299df988d45076f1
Neyrin <isa.areschoug@gmail.com>
2018-11-26 19:03:21 +0100

    Fixed typo

--- lang/sv.php
+++ lang/sv.php
@@ -16,8 +16,7 @@ return [
   'history-header' => 'Historien om FS1',
   'history-cont' => 'Saab startade 1937 i Trollhättan som en flygplansproducent för att tillverka flygplan till det svenska flygvapnet, precis som Europa förberedde sig för ett nytt krig. Företagets fulla namn var Svenska Aeroplan Aktiebolaget, vilket gav världen akronym Saab. Huvudkontoret flyttade snart till den svenska universitetsstaden Linköping, där detta enorma företag har sin största verksamhet idag. Det var där 1945, bland olika drömmar om diversifiering, som ett projekt för att utveckla bilar började. Idag börjar vi ett nytt kapitel. Snabb säkerhet, FS. Är framtidens motorcykel.',
   'auto-header' => 'Fullständigt Autonom',
-  'auto-cont' => '
-Autonoma bilar kombinerar en mängd olika sensorer för att uppfatta sina omgivningar, såsom radar, datasyn, Lidar, Sonar, GPS, Odometry och tröghetsmätningsenheter. En självkörande bil, även känd som en robotbil, autonom bil eller förarlös bil är ett fordon som kan känna av sin miljö och flytta med liten eller ingen mänsklig inmatning.',
+  'auto-cont' => 'Autonoma bilar kombinerar en mängd olika sensorer för att uppfatta sina omgivningar, såsom radar, datasyn, Lidar, Sonar, GPS, Odometry och tröghetsmätningsenheter. En självkörande bil, även känd som en robotbil, autonom bil eller förarlös bil är ett fordon som kan känna av sin miljö och flytta med liten eller ingen mänsklig inmatning.',
   'break-header' => 'Bromssensorer',
   'break-cont' => 'Nå toppfart eller ta snäva kurvor, Lucid Airs perfekt kalibrerade luftfjädring, avancerade dämpningsteknik och låga tyngdpunkt levererar precisionhantering och smidig körning.',
   'phone-header' => 'Smartphone App',
