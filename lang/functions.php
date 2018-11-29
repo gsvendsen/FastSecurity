dbc1b9cfca7ad786547b5d5269ddb230a4bd9b65
Gustav Svendsen <gsvendsen99@gmail.com>
2018-11-29 20:56:05 +0100

    Added documentation

--- lang/functions.php
+++ lang/functions.php
@@ -2,18 +2,15 @@
 
 declare(strict_types=1);
 
+/* Function that fetches translation using two keys */
 function translate(string $key1, string $key2):string {
+
   $languages = [
     'sv' => __DIR__.'/sv.php',
     'en' => __DIR__.'/en.php',
   ];
 
-  if(isset($_GET['lang'])){
-
-    $_SESSION['lang'] = $_GET['lang'];
-
-  }
-
+  /* Fetches language data array using lang session */
   $selectedLanguage = require $languages[$_SESSION['lang']];
 
   return $selectedLanguage[$key1][$key2];
