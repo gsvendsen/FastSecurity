53011329df5bc1c4287d66b60ec3a228028aebbe
Gustav Svendsen <gsvendsen99@gmail.com>
2018-11-30 13:16:31 +0100

    Added lang query check

--- lang/functions.php
+++ lang/functions.php
@@ -10,9 +10,18 @@ function translate(string $key1, string $key2):string {
     'en' => __DIR__.'/en.php',
   ];
 
+  /* Uses language of lang query if there is one */
+  if(isset($_GET['lang'])){
+      $lang = filter_var($_GET['lang'], FILTER_SANITIZE_STRING);
+      /* Check if lang query exists in languages array and uses it */
+      foreach ($languages as $language => $value) {
+          if($lang === $language){
+              $_SESSION['lang'] = $lang;
+          }
+      }
+  }
+
   /* Fetches language data array using lang session */
   $selectedLanguage = require $languages[$_SESSION['lang']];
-
   return $selectedLanguage[$key1][$key2];
-
 }
